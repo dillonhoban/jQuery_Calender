0be1b21c039b5e625bae3c562aabb9d51bba7b3a
Dillon H <dhobs21@gmail.com>
2021-03-02 12:03:17 -0800

    adding fixed code, working finally!

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,110 +1,48 @@
 $(document).ready(function () {
 
   // Pull the date and time from moment.js
-  $(".lead").append("Today is " + moment().format("dddd MMMM Do YYYY"));
+  $("#currentDay").append(moment().format("dddd MMMM Do YYYY"));
 
-  // Setting variables for the color changer
-  var timeFormat = "hh:mm:ss";
-  var timeArray = [{
-      textarea: $("#9"),
-      begin: "09:00:00",
-      end: "09:59:59"
-    },
-    {
-      textarea: $("#10"),
-      begin: "10:00:00",
-      end: "10:59:59"
-    },
-    {
-      textarea: $("#11"),
-      begin: "11:00:00",
-      end: "11:59:59"
-    },
-    {
-      textarea: $("#12"),
-      begin: "12:00:00",
-      end: "12:59:59"
-    },
-    {
-      textarea: $("#13"),
-      begin: "13:00:00",
-      end: "13:59:59"
-    },
-    {
-      textarea: $("#14"),
-      begin: "14:00:00",
-      end: "14:59:59"
-    },
-    {
-      textarea: $("#15"),
-      begin: "15:00:00",
-      end: "15:59:59"
-    },
-    {
-      textarea: $("#16"),
-      begin: "16:00:00",
-      end: "16:59:59"
-    },
-    {
-      textarea: $("#17"),
-      begin: "17:00:00",
-      end: "17:59:59"
-    },
-    {
-      textarea: $("#18"),
-      begin: "18:00:00",
-      end: "18:59:59"
-    },
-  ];
+  let currentHour = parseInt(moment().format("HH"));
 
-  // Creating the function to change to text area colors 
-  function colorChanger() {
-    for (var i = 0; i < timeArray.length; i++) {
-      var colorPast = moment(timeArray[i].begin, timeFormat);
-      var colorFuture = moment(timeFormat[i].end, timeFormat);
-      if (moment().isBetween(colorPast, colorFuture)) {
-        timeArray[i].textarea.addClass("present");
-      } else if (moment().isBefore(colorPast)) {
-        timeArray[i].textarea.addClass("past");
-      } else if (moment().isAfter(colorFuture)) {
-        timeArray[i].textarea.addClass("future");
-      };
+  //Change textarea background color based on time
+  let backgroundColor = function () {
+    let textAreas = $(".description");
+    for (let i = 0; i < textAreas.length; i++) {
+      let textAreaID = parseInt(textAreas[i].id);
+      if (textAreaID < currentHour) {
+        textAreas[i].classList.add("past");
+      } else if (textAreaID > currentHour) {
+        textAreas[i].classList.add("future");
+      } else {
+        textAreas[i].classList.add("present");
+      }
     }
   };
-
+  backgroundColor();
 
   // Creating the on click button to save date to Local Storage
   $(".saveBtn").click(function () {
-    var addValueID = $(this).prev().attr("id");
-    var addValue = $(this).prev().val();
-    localStorage.setItem(addValueID, addValue);
+    let addDataID = $(this).prev().attr("id");
+    let addData = $(this).prev().children().val();
+    localStorage.setItem(addDataID, addData);
   });
 
-  // // Getting the local data
-  // var rowsID = $(".row");
-  // for (i = 0; i < rowsID.length; i++) {
-  //   $("#" + i).val(localStorage.getItem(i))
-  // };
+  //Get local storage data
+  // Get ID
+  // Get data
+  //Update the text area
 
-  // var timeBlockArray = $(".time-slot");
-  // timeBlockArray.each(function (index) {
-  //   var $timeBlock = $(timeBlockArray[index]);
+  function getLocalData() {
 
-  //   // getting the id 
-  //   var id = $timeBlock.find("textarea").attr("id");
-  //   console.log("****id", id);
-
-  //   // getting the data associated with each id
-  //   var data = localStorage.getItem(`hour-${id}`);
-  //   console.log("****data", data);
-
-  //   // Updating the textarea with the data found in local storage
-  //   if (data) {
-  //     $timeBlock.find("textarea").val(data);
-  //   } else {
-  //     $timeBlock.find("textarea").val("");
-  //   }
-
-  // });
+    //If null, won't add anything
+    for (j = 9; j < 18; j++) {
+      let data = localStorage.getItem(`${j}`);
+      console.log(data);
+      $(`#${j} textarea`).val(data);
+    };
+  };
+  //Calls function upon opening page
+  getLocalData();
 
 });
--- README.md
+++ README.md
@@ -19,7 +19,7 @@ This site’s HTML was validated using a third party service found [here.](https
   <img src="https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white" alt="HTML-Logo">
   <img src="https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white" alt="CSS-Logo">
   <img src="https://img.shields.io/badge/bootstrap%20-%23563D7C.svg?&style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap-Logo">
-  <img src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
+  <img src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="Javascript-Logo">
   <img src="https://img.shields.io/badge/jquery%20-%230769AD.svg?&style=for-the-badge&logo=jquery&logoColor=white" alt="jQuery-Logo">
 </p>
 
